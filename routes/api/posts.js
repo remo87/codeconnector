d20019883254c8fb304b3f2f632e8ffd6f7a4dff
Ricardo Martinez <ricardo.martinez@daktronics.com>
2019-01-21 13:48:43 -0600

    delete post

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 
 //models
 const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
 
 //validators
 const ValidatePostInput = require('../../validation/post');
@@ -16,6 +17,29 @@ router.get('/test', (req, res) => {
     res.json({
         msg: 'posts works'
     })
+});
+
+// @route   GET api/post
+// @desc    Get posts
+// @access  public
+router.get('/', (req, res) => {
+    Post.find()
+        .sort({ date: -1 })
+        .then(posts => res.json(posts))
+        .catch(err => res.status(404).json({ nopostfound: 'No posts found.' }));
+});
+
+// @route   GET api/post/:id
+// @desc    Get post by id
+// @access  public
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (!post) {
+                return res.status(404).json({postnotfound: 'No post found with that id.'})
+            }
+            return res.json(post)})
+        .catch(err => res.status(404).json({ nopostfound: 'No post found with that id.' }));
 });
 
 // @route   POST api/post
@@ -40,5 +64,23 @@ router.post('/', passport.authenticate('jwt', { session: false }), (req, res) =>
             res.json(newPost);
         })
 });
+
+// @route   DELETE api/post
+// @desc    Delete a Post
+// @access  private
+router.delete('/:id', passport.authenticate('jwt', { session: false }), (req, res) => {
+    Post.findById(req.params.id)
+        .then(post => {
+            if (post.user.toString() !== req.user.id) {
+                return res.status(401).json({
+                    notauthorized: 'The user is not authorized.'
+                });
+            }
+
+            post.remove().then(()=> res.json({success: true}));
+        })
+        .catch(err => res.status(404).json({postnotfound: 'No post found.'}));
+});
+
 
 module.exports = router;
