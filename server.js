8ceb08273efd5abf59ce7fc8f19d39eafa522217
Ricardo Martinez <ricardo.martinez@daktronics.com>
2019-01-22 07:13:38 -0600

    add react boilerplate add concurrently

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ app.use('/api/users', users);
 app.use('/api/profile', profiles);
 app.use('/api/post', posts);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`)
