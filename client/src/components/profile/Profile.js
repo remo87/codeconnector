73d0f085e287fab69a46624823cb6d3e7f632a5f
Ricardo Martinez <remo87@gmail.com>
2019-02-06 18:12:43 -0500

    add profile github and notfound

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -18,6 +18,12 @@ class Profile extends Component {
     }
   }
 
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.profile.profile === null && nextProps.profile.loading) {
+      this.props.history.push("/not-found");
+    }
+  }
+
   render() {
     const { profile, loading } = this.props.profile;
     let profileContent;
@@ -34,8 +40,13 @@ class Profile extends Component {
           </div>
           <ProfileHeader profile={profile} />
           <ProfileAbout profile={profile} />
-          <ProfileCreds education={profile.education} experience={profile.experience} />
-          <ProfileGithub />
+          <ProfileCreds
+            education={profile.education}
+            experience={profile.experience}
+          />
+          {profile.githubusername ? (
+            <ProfileGithub username={profile.githubusername} />
+          ) : null}
         </div>
       );
     }
