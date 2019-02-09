dca794ef766960e90b018a3534aa10a2a248a913
Ricardo Martinez <remo87@gmail.com>
2019-02-09 08:36:12 -0500

    configure deployment

--- server.js
+++ server.js
@@ -1,40 +1,54 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyparser = require('body-parser');
-const passport = require('passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyparser = require("body-parser");
+const passport = require("passport");
+const path = require("path");
 
-
-const users = require('./routes/api/users');
-const profiles = require('./routes/api/profiles');
-const posts = require('./routes/api/posts');
+const users = require("./routes/api/users");
+const profiles = require("./routes/api/profiles");
+const posts = require("./routes/api/posts");
 
 const app = express();
 
 //configure body parser middleware
-app.use(bodyparser.urlencoded({extended: false}));
+app.use(bodyparser.urlencoded({ extended: false }));
 app.use(bodyparser.json());
 
 //db config
-const db = require('./config/keys').mongoURI;
+const db = require("./config/keys").mongoURI;
 
 //connect to mongo db
 mongoose
-    .connect(db)
-    .then(() => {console.log('connected to mongo db')})
-    .catch(err => {console.log(`error connecting to mongo ${err}`)});
+  .connect(db)
+  .then(() => {
+    console.log("connected to mongo db");
+  })
+  .catch(err => {
+    console.log(`error connecting to mongo ${err}`);
+  });
 
 //Passport middleware
 app.use(passport.initialize());
 
 //Passport Config
-require('./config/passport')(passport);
+require("./config/passport")(passport);
 
-app.use('/api/users', users);
-app.use('/api/profile', profiles);
-app.use('/api/post', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profiles);
+app.use("/api/post", posts);
+
+//Serve Static assets if in production
+if (process.env.NODE_ENV === "production") {
+  //set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`);
 });
